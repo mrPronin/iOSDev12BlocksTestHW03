c1174ebe2c2ee273dbae3cef7caddd881da04fe3
Pronin Alexander <pronin.alx@gmail.com>
2014-02-10 18:50:18 +0200

    Initial Commit

---
+++ 12BlocksTestHW03Tests/_2BlocksTestHW03Tests.m
@@ -0,0 +1,34 @@
+//
+//  _2BlocksTestHW03Tests.m
+//  12BlocksTestHW03Tests
+//
+//  Created by Aleksandr Pronin on 10.02.14.
+//  Copyright (c) 2014 Aleksandr Pronin. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _2BlocksTestHW03Tests : XCTestCase
+
+@end
+
+@implementation _2BlocksTestHW03Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
